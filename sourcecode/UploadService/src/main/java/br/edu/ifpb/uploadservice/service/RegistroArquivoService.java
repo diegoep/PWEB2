181256a0f1a7a14eb5836537f746750a5d36692b
lyndemberg <lyndembergbatista@outlook.com>
2018-06-08 16:14:49 -0300

    integrando arquivo repository com o registro

--- sourcecode/UploadService/src/main/java/br/edu/ifpb/uploadservice/service/RegistroArquivoService.java
+++ sourcecode/UploadService/src/main/java/br/edu/ifpb/uploadservice/service/RegistroArquivoService.java
@@ -4,6 +4,7 @@ import br.edu.ifpb.uploadservice.domain.Arquivo;
 import br.edu.ifpb.uploadservice.domain.RegistroArquivo;
 import br.edu.ifpb.uploadservice.domain.RegistroArquivo.EnvioArquivoStatus;
 import br.edu.ifpb.uploadservice.domain.ReservaEspaco;
+import br.edu.ifpb.uploadservice.repository.ArquivoRepository;
 import br.edu.ifpb.uploadservice.repository.RegistroArquivoRepository;
 import br.edu.ifpb.uploadservice.repository.ReservaEspacoRepository;
 import br.edu.ifpb.uploadservice.service.erros.CaminhoInexistente;
@@ -22,10 +23,12 @@ import java.util.List;
 public class RegistroArquivoService {
     private final RegistroArquivoRepository registroArquivoRepository;
     private final ReservaEspacoRepository reservaEspacoRepository;
+    private final ArquivoRepository arquivoRepository;
 
-    public RegistroArquivoService(RegistroArquivoRepository registroArquivoRepository, ReservaEspacoRepository reservaEspacoRepository) {
+    public RegistroArquivoService(RegistroArquivoRepository registroArquivoRepository, ReservaEspacoRepository reservaEspacoRepository, ArquivoRepository arquivoRepository) {
         this.registroArquivoRepository = registroArquivoRepository;
         this.reservaEspacoRepository = reservaEspacoRepository;
+        this.arquivoRepository = arquivoRepository;
     }
 
     public List<RegistroArquivo> listarRegistros(){
@@ -66,6 +69,7 @@ public class RegistroArquivoService {
         arquivo.setFormato(FilenameUtils.getExtension(nome));
         arquivo.setTamanho(tamanho);
         arquivo.setLocalArmazenamento(reserva.getLocalArmazenamento());
+        arquivoRepository.save(arquivo);
 
         RegistroArquivo registro = new RegistroArquivo();
         registro.setArquivo(arquivo);
