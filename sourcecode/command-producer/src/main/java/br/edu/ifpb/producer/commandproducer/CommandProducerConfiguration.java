a9f9aa340a163857239cfb833cfd0248850812d9
Diego Pessoa <diegopessoa12@gmail.com>
2018-08-02 13:41:58 -0300

    versao inicial

--- sourcecode/command-producer/src/main/java/br/edu/ifpb/producer/commandproducer/CommandProducerConfiguration.java
+++ sourcecode/command-producer/src/main/java/br/edu/ifpb/producer/commandproducer/CommandProducerConfiguration.java
@@ -1,8 +1,10 @@
 package br.edu.ifpb.producer.commandproducer;
 
+import br.edu.ifpb.producer.service.ReplyConsumer;
 import io.eventuate.tram.commands.common.ChannelMapping;
 import io.eventuate.tram.commands.common.DefaultChannelMapping;
 import io.eventuate.tram.commands.consumer.CommandDispatcher;
+import io.eventuate.tram.messaging.consumer.MessageConsumer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,6 +22,11 @@ public class CommandProducerConfiguration {
     @Bean
     public ChannelMapping channelMapping(CommandConsumerParameters parameters) {
         return new DefaultChannelMapping(Collections.singletonMap("UploadServiceAggregate", parameters.getCommandChannel()));
+    }
+
+    @Bean
+    public ReplyConsumer myReplyConsumer(CommandConsumerParameters parameters, MessageConsumer messageConsumer) {
+        return new ReplyConsumer(messageConsumer, parameters.getReplyChannel());
     }
 
 }
